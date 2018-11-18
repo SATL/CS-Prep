3587ccc2fe87545ba1ef52f220e2ddd2f344d549
Eslem <j.eslem03@gmail.com>
2018-11-18 15:42:20 +0100

    heap, bit operation

---
+++ src/BitsOperations.java
@@ -0,0 +1,43 @@
+public class BitsOperations {
+
+
+    //[ 1, 2,3,1,2]
+    /*
+        001
+        010
+        011
+        001
+        010
+
+        011
+
+     */
+    public static int lonelyInteger(int[] args){
+        int val=0;
+        for(int i=0; i<args.length; ++i){
+            val^=args[i];
+        }
+        return val;
+    }
+
+
+    public static void main(String[] args){
+        int a = 0x0005; //5   0101
+        int b = 0x0009; //10  1001
+
+        int add = a & b; // 0001 = 1
+        System.out.println("a&b="+add);
+
+        int or = a |b; //1101 = 13
+        System.out.println("a|b="+or);
+
+        int xor = a^b; //1100 12
+        System.out.println("a^b="+xor);
+
+        int inv = ~a; //1010
+        System.out.println("~a="+inv);
+
+        int[] arr = new int[]{1,2,3,1,2};
+        System.out.println("Lonely integer "+lonelyInteger(arr));
+    }
+}
---
+++ src/Heap.java
@@ -0,0 +1,59 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Heap {
+
+
+    public static void addValue(int value, PriorityQueue<Integer> lowerQueue, PriorityQueue<Integer> upperQueue){
+        if(lowerQueue.size() == 0 || value < lowerQueue.peek()){
+            lowerQueue.add(value);
+        }else{
+            upperQueue.add(value);
+        }
+    }
+
+    public static void balanceQueues(PriorityQueue<Integer> lowerQueue, PriorityQueue<Integer> upperQueue){
+        PriorityQueue<Integer> smaller = lowerQueue.size() < upperQueue.size()?lowerQueue:upperQueue;
+        PriorityQueue<Integer> bigger = lowerQueue.size() > upperQueue.size()?lowerQueue:upperQueue;
+
+        if(bigger.size() - smaller.size() > 2 ){
+            smaller.add(bigger.poll());
+        }
+
+    }
+    public static double getMedian(PriorityQueue<Integer> lowerQueue, PriorityQueue<Integer> upperQueue){
+        PriorityQueue<Integer> smaller = lowerQueue.size() < upperQueue.size()?lowerQueue:upperQueue;
+        PriorityQueue<Integer> bigger = lowerQueue.size() > upperQueue.size()?lowerQueue:upperQueue;
+        if(lowerQueue.size() == upperQueue.size()){
+            return ( (double) lowerQueue.peek() + upperQueue.peek() ) /2;
+        }else{
+            return bigger.peek();
+        }
+    }
+
+    public static double[] getMedian(int[] arr){
+        PriorityQueue<Integer> lowerQueue = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> upperQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return -1 * o1.compareTo(o2);
+            }
+        });
+
+        double[] medians = new double[arr.length];
+
+        for(int i=0; i<arr.length; ++i){
+            addValue(arr[i], lowerQueue, upperQueue);
+            balanceQueues(lowerQueue, upperQueue);
+            medians[i] = getMedian(lowerQueue, upperQueue);
+        }
+
+        return medians;
+
+    }
+
+
+    public static void main(String[] args){
+
+    }
+}
